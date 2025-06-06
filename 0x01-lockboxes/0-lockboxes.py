671f906dab8cc8831458815c0573c1d3062bea7f
Nepomunezero <j.munezero1@alustudent.com>
2025-06-06 10:32:38 +0200

    updated files

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+def canUnlockAll(boxes):
+    if not isinstance(boxes, list) or len(boxes) == 0:
+        return False
+
+    n = len(boxes)
+    opened = set([0])  # Start with box 0 opened
+    stack = [0]        # DFS stack
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if 0 <= key < n and key not in opened:
+                opened.add(key)
+                stack.append(key)
+
+    return len(opened) == n
