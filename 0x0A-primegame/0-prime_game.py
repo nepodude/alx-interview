3a68e7247994073d63f8b3f5953b66a265549cdd
Nepomunezero <j.munezero1@alustudent.com>
2025-06-26 19:46:32 +0200

    debugged file

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,33 +7,38 @@ file to determine the winner
 
 def isprime(n):
     """ Checks if the number is a prime """
-    for i in range(n):
-        if n % i == 1 and i != 1:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
 def isWinner(x, nums):
     """ Checks howmany prime integers less than numbers in nums """
+    if x < 1 or not nums:
+        return None
     benMaria = []
     Marias = 0
     Bens = 0
     for i in range(x):
         howmanyprimes = 0
-        for j in range(nums[i] + 1):
+        for j in range(1, nums[i] + 1):
             if isprime(j):
-                howmanyprimes = howmanyprimes + 1
-        if howmanyprimes % 2 == 0:
+                howmanyprimes += 1
+        if howmanyprimes % 2 == 1:
             benMaria.append("Ben")
         else:
             benMaria.append("Maria")
     for i in benMaria:
         if i == "Ben":
-            Bens = Bens + 1
+            Bens += 1
         elif i == "Maria":
-            Marias = Marias + 1
+            Marias += 1
     if Bens > Marias:
         return "Ben"
     elif Marias > Bens:
         return "Maria"
     else:
+        print("none")
         return None
+
+isWinner(5, [2, 5, 1, 4, 3])
